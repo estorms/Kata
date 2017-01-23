297e4d6962c70cea5ce56538d47327943f4bd8b6
Elizabeth Sanger <libsang@gmail.com>
2017-01-23 12:54:42 -0600

    num descending order added

--- ChallengeLib.cs
+++ ChallengeLib.cs
@@ -70,7 +70,14 @@ namespace Challenges
             
         }
 
+// Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Descending order means that you take the highest digit and place the next highest digit immediately after it.
 
+public int DescendingOrder (int num) {
+ var nums = num.ToString().ToCharArray().OrderByDescending(i => i).ToArray();
+    return Convert.ToInt32(new string(nums));
+    
+
+}
         public int IndexOfOdd(string numbers)
         {
             numbers = numbers.Replace(" ", String.Empty);
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Challenges
     {
         public static void Main(string[] args)
         {
-            
+
             //check dupes
             ChallengesLib challengesLib = new ChallengesLib();
             // Console.WriteLine(challengesLib.CheckDupes(" "));
@@ -15,17 +15,22 @@ namespace Challenges
             //   Console.WriteLine(challengesLib.CheckDupes("aabcDde"));
             // Console.WriteLine(challengesLib.CheckDupes("Indivisibility"));
             // Console.WriteLine(challengesLib.CheckDupes("hulkout"));
+
+            //Description: index of odd
+
+            //  Console.WriteLine(challengesLib.IndexOfOdd("2 4 5"));
+
+            //Description: index of first odd or even in a sequence
+
+            // Console.WriteLine(challengesLib.OddOrEven(("2 4 7 8 10")));
+            // Console.WriteLine(challengesLib.OddOrEven(("1 2 1 1")));
+            // Console.WriteLine(challengesLib.OddOrEven(("1 1 1 1 11 110")));
+
+            //Description: order string of nums by descending
             
-            //index of odd
-         Console.WriteLine(challengesLib.IndexOfOdd("2 4 5"));
+            Console.WriteLine(challengesLib.DescendingOrder(6874));
 
-         //index of first odd or even in a sequence
 
-          Console.WriteLine(challengesLib.OddOrEven(("2 4 7 8 10")));
-           Console.WriteLine(challengesLib.OddOrEven(("1 2 1 1")));
-              Console.WriteLine(challengesLib.OddOrEven(("1 1 1 1 11 110")));
-           
-         
         }
     }
 }
