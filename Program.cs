6c29c13d43c0313d6cfd557bd31db18fa18856dc
Elizabeth Sanger <libsang@gmail.com>
2017-02-01 15:08:43 -0600

    updated program

--- Program.cs
+++ Program.cs
@@ -33,29 +33,6 @@ namespace Challenges
             Console.WriteLine(challengesLib.AscendingOrder(6874));
             Console.WriteLine(challengesLib.DescendingAlt(6874));
 
-            for (var i = 0; i <= 25; i++)
-            {
-
-                if (i % 5 == 0 && i % 3 == 0)
-                {
-                    Console.WriteLine("fizzbuzz");
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("fizz");
-                }
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("buzz");
-                }
-
-                else
-                {
-                    Console.WriteLine(i);
-                }
-
-            }
-
             //Description: Fibonacci Sequence
 
             for (var i = 0; i <= 20; i++)
